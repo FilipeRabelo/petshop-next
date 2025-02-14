81f07944ea332f201d13e9898261dcd1c242c388
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2025-02-14 14:54:47 -0300

    projeto-petshop-next

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,19 @@
+
+
+
+const brands = [
+  { name: "Royal Canin", logo: royal },
+  { name: "Golden", logo: golden },
+  { name: "Primier", logo: primier },
+  { name: "Formula Natural", logo: natural },
+  { name: "Whiskas", logo: whiskas },
+  { name: "Golden", logo: golden },
+]
+
+export function Footer(){
+  return(
+    <section>
+
+    </section>
+  )
+}
--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -53,6 +53,16 @@ export function Services() {
     }
   })
 
+
+  function scrollPrev() {      // seta para voltar  
+    emblaApi?.scrollPrev();
+  }
+
+  function scrollNext() {
+    emblaApi?.scrollNext();   // seta para frente
+  }
+
+
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-4">
@@ -67,8 +77,8 @@ export function Services() {
               {services.map((item, index) => (
                 <div key={index} className='flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
                   <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col'>
-                    
-                    <div className='flex-1 flex items-start justify-between'>                      
+
+                    <div className='flex-1 flex items-start justify-between'>
                       <div className='flex gap-3 '>
                         {/* <span className='text-3xl'>{item.icon}</span> */}
                         <div>
@@ -87,20 +97,20 @@ export function Services() {
 
                     <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
                       <div className='flex items-center gap-2 text-sm'>
-                        <Clock className='w-4 h-4'/>
+                        <Clock className='w-4 h-4' />
                         <span>{item.duration}</span>
                       </div>
 
-                      <a 
+                      <a
                         data-aos="fade-up"
                         data-aos-delay="500"
                         target='_blank'
                         href={`https://wa.me/5531982942423?text=Olá vim pelo site e gostaria de mais informações`}
 
-                        className='flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300'  
+                        className='flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300'
                       >
                         Contato
-                        <WhatsappLogo className='w-5 h-5'/>
+                        <WhatsappLogo className='w-5 h-5' />
                       </a>
                     </div>
 
@@ -110,6 +120,23 @@ export function Services() {
 
             </div>
           </div>
+
+          <button
+            onClick={scrollPrev}
+            className='bg-white flex items-center justify-center rounded-full 
+                      shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10'
+          >
+            <ChevronLeft className='w-6 h-6 text-gray-600' />
+          </button>
+
+          <button
+            onClick={scrollNext}
+            className='bg-white flex items-center justify-center rounded-full 
+                      shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10'
+          >
+            <ChevronRight className='w-6 h-6 text-gray-600' />
+          </button>
+
         </div>
 
       </div>
