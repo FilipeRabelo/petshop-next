60707602eb6188b21fafe008f938512f4de67f2c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2025-02-14 15:24:04 -0300

    projeto-petshop-next

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,4 +1,9 @@
-
+import golden from '../../../public/golden.png'
+import royal from '../../../public/royal.png'
+import primier from '../../../public/primier.png'
+import whiskas from'../../../public/whiskas.png'
+import natural from '../../../public/natural.png'
+import Image from 'next/image'
 
 
 const brands = [
@@ -10,10 +15,45 @@ const brands = [
   { name: "Golden", logo: golden },
 ]
 
-export function Footer(){
-  return(
-    <section>
+export function Footer() {
+  return (
+    <section className='bg-[#E84C3D] py-16 text-white'>
+      <div className='container mx-auto px-4'>
 
+        <div className='border-b border-white/20 pb-8'>
+          <h4 className='text-3xl font-semibold mb-8 text-center'>Nossos parceiros</h4>
+
+          <div className='grid grid-cols-2 lg:grid-cols-6 gap-8'>
+            {brands.map((item, index) => (
+              <div key={index} className='bg-white p-4 rounded-lg flex items-center justify-center'>
+                <Image
+                  src={item.logo}
+                  alt={item.name}
+                  width={100}
+                  height={50}
+                  quality={100}
+                  style={{
+                    width: 'auto',
+                    height: 'auto'
+                  }}
+                  className='object-contain'
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+        <footer className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5'>
+          <div>
+            <h3 className='text-2xl font-semibold mb-2'>Pet Shop</h3>
+            <p>
+              Cuidando do seu melhor amigo com amor e dedicação
+            </p>
+          </div>
+        </footer>
+
+      </div>
     </section>
   )
 }
